264381c4cf9d359ed8c6258982c0623525198292
Chao Ma <mc8305@gmail.com>
2022-05-15 15:28:34 -0700

    add timing for benchmark

--- benches/bench_pippenger_msm.rs
+++ benches/bench_pippenger_msm.rs
@@ -1,25 +1,30 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use wasm_zkp_challenge::msm::{compute_msm, compute_pippenger, compute_pippenger_affine,compute_msm_affine, generate_msm_inputs};
+mod perf;
 
 fn bench_pippenger_msm(c: &mut Criterion) {
     let mut group = c.benchmark_group("bench_pippenger_msm");
-    for size in [8, 10, 12, 14].iter() {
+    for size in [8, 10 ].iter() {
         let (point_vec, scalar_vec) = generate_msm_inputs(1 << size);
         let point_vec = black_box(point_vec);
         let scalar_vec = black_box(scalar_vec);
         let input = (point_vec, scalar_vec);
 
         group.bench_with_input(
-            BenchmarkId::from_parameter(format!("input vector length: 2^{}", size)),
+            BenchmarkId::from_parameter(format!("input_vector_length_2_{}", size)),
             &input,
             |b, input| {
                 b.iter(|| {
-                    compute_msm_affine(input.0.clone(), input.1.clone());
+                    compute_pippenger_affine(input.0.clone(), input.1.clone());
                 })
             },
         );
     }
 }
 
-criterion_group!(benches, bench_pippenger_msm);
+criterion_group!{
+    name = benches;
+    config = Criterion::default().with_profiler(perf::FlamegraphProfiler::new(100));
+    targets = bench_pippenger_msm
+}
 criterion_main!(benches);
--- src/msm.rs
+++ src/msm.rs
@@ -1,7 +1,7 @@
 use ark_ec::{AffineCurve, ProjectiveCurve, msm};
 use ark_ff::{PrimeField, UniformRand, Zero, FpParameters, BigInteger};
 use ark_bls12_381::G1Affine;
-
+use std::time::{Duration,Instant};
 
 pub fn generate_msm_inputs(size: usize)
 -> (
@@ -36,6 +36,7 @@ pub fn compute_msm_affine(
 ) -> <G1Affine as AffineCurve>::Projective
 {
     let (window_buckets,s) = msm::VariableBaseMSM::multi_scalar_mul_batch_affine(point_vec.as_slice(), scalar_vec.as_slice());
+    let start = Instant::now();
     let mut Gs:Vec<_> = ark_std::cfg_into_iter!(window_buckets).map(|pts|{
         let zero =<G1Affine as AffineCurve>::Projective::zero();
         let mut res = zero.clone();
@@ -47,6 +48,8 @@ pub fn compute_msm_affine(
         });
         res
     }).collect();
+    let duration = start.elapsed();
+    println!("baseline+affine  | compute_msm_affine | batch affine buckets: {:?}", duration);
     let lowest = *Gs.first().unwrap();
     // We're traversing windows from high to low.
     lowest
@@ -75,9 +78,12 @@ pub fn compute_pippenger_affine(
     scalar_vec: Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>,
 ) -> <G1Affine as AffineCurve>::Projective {
     let (tmp, s)= msm::VariableBaseMSM::pippenger_batch_affine(point_vec.as_slice(),scalar_vec.as_slice());
+    let start = Instant::now();
     let mut Gs:Vec<_> = ark_std::cfg_into_iter!(tmp).map(|pts|{
         G1Affine::batch_affine_addition(pts).into_projective()
     }).collect();
+    let duration = start.elapsed();
+    println!("pippenger+batch | compute_pippenger_affine | batch_affine_addition {:?}", duration);
     let lowest = *Gs.first().unwrap();
     // We're traversing windows from high to low.
     lowest
@@ -99,7 +105,7 @@ pub fn compute_pippenger_affine(
 #[test]
 fn test() {
     use ark_ff::BigInteger;
-    let size = 1<<12;
+    let size = 1<<14;
     let (point_vec, scalar_vec) = generate_msm_inputs(size);
     //let scalar = <<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt::from_bits_le(&[true,false]);
     let ww = point_vec.clone();
@@ -107,7 +113,7 @@ fn test() {
     let res2 = compute_pippenger(point_vec.clone(), scalar_vec.clone());
     let res3 = compute_pippenger_affine(point_vec.clone(), scalar_vec.clone());
     let res4 = compute_msm_affine(point_vec.clone(), scalar_vec.clone());
-    println!("baseline = {:?}\n", res1.into_affine());
+    println!("\n baseline = {:?}\n", res1.into_affine());
     println!("pippenger = {:?}\n", res2.into_affine());
     println!("pippenger-affine= {:?}\n", res3.into_affine());
     println!("msm-affine= {:?}\n", res4.into_affine());
