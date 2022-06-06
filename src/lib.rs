e15ec18106952f922cf2ccc2a1e8d27315cadf29
Chao Ma <mc8305@gmail.com>
2022-06-05 22:57:06 -0700

    test(test new optimization): add benchmark test for new optimization

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,20 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn bench_fft_and_ifft(input_domain_dim: usize, output_domain_dim: usize) {
-    let (rand_evaluation_domain, output_domain) = fft::generate_random_evaluation(input_domain_dim, output_domain_dim);
+    let (rand_evaluation_domain, output_domain) =
+        fft::generate_random_evaluation(input_domain_dim, output_domain_dim);
 
     fft::compute_fft_and_ifft(rand_evaluation_domain, output_domain);
 }
 
 #[wasm_bindgen]
 pub fn bench_msm(size: usize) {
-    let (point_vec, scalar_vec) = msm::generate_msm_inputs(1<<size);
-    msm::compute_pippenger_affine(point_vec, scalar_vec);
+    let (point_vec, scalar_vec) = msm::generate_msm_inputs(1 << size);
+    msm::compute_msm(point_vec, scalar_vec);
 }
 
 #[wasm_bindgen]
 pub fn bench_pairing(size: usize) {
-    let (g1_rand_vec, g2_rand_vec) = pairing::generate_pairing_inputs(1<<size);
+    let (g1_rand_vec, g2_rand_vec) = pairing::generate_pairing_inputs(1 << size);
     pairing::compute_billinearity(g1_rand_vec, g2_rand_vec);
 }
--- src/msm.rs
+++ src/msm.rs
@@ -1,13 +1,14 @@
-use ark_ec::{AffineCurve, ProjectiveCurve, msm};
-use ark_ff::{PrimeField, UniformRand, Zero, FpParameters, BigInteger};
 use ark_bls12_381::G1Affine;
-use std::time::{Duration,Instant};
+use ark_ec::{msm, AffineCurve, ProjectiveCurve};
+use ark_ff::{BigInteger, FpParameters, PrimeField, UniformRand, Zero};
+use std::time::{Duration, Instant};
 
-pub fn generate_msm_inputs(size: usize)
--> (
-    Vec<<<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::Affine>, 
+pub fn generate_msm_inputs(
+    size: usize,
+) -> (
+    Vec<<<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::Affine>,
     Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>,
-){
+) {
     let mut rng = ark_std::test_rng();
 
     let scalar_vec = (0..size - 1)
@@ -16,7 +17,10 @@ pub fn generate_msm_inputs(size: usize)
     let point_vec = (0..size)
         .map(|_| <G1Affine as AffineCurve>::Projective::rand(&mut rng))
         .collect::<Vec<_>>();
-    let point_vec = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&point_vec);
+    let point_vec =
+        <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(
+            &point_vec,
+        );
 
     (point_vec, scalar_vec)
 }
@@ -25,96 +29,32 @@ pub fn generate_msm_inputs(size: usize)
 pub fn compute_msm(
     point_vec: Vec<<<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::Affine>,
     scalar_vec: Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>,
-) -> <G1Affine as AffineCurve>::Projective
-{
+) -> <G1Affine as AffineCurve>::Projective {
     msm::VariableBaseMSM::multi_scalar_mul(point_vec.as_slice(), scalar_vec.as_slice())
 }
 
-pub fn compute_msm_affine(
-    point_vec: Vec<<<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::Affine>,
-    scalar_vec: Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>,
-) -> <G1Affine as AffineCurve>::Projective
-{
-    let (window_buckets,s) = msm::VariableBaseMSM::multi_scalar_mul_batch_affine(point_vec.as_slice(), scalar_vec.as_slice());
-    let start = Instant::now();
-    let mut Gs:Vec<_> = ark_std::cfg_into_iter!(window_buckets).map(|pts|{
-        let zero =<G1Affine as AffineCurve>::Projective::zero();
-        let mut res = zero.clone();
-        let mut running_sum = zero;
-        pts.into_iter().rev().for_each(|b| {
-            let tmp = G1Affine::batch_affine_addition(b).into_projective();
-            running_sum += tmp;
-            res += &running_sum;
-        });
-        res
-    }).collect();
-    let duration = start.elapsed();
-    println!("baseline+affine  | compute_msm_affine | batch affine buckets: {:?}", duration);
-    let lowest = *Gs.first().unwrap();
-    // We're traversing windows from high to low.
-    lowest
-        + &Gs[1..]
-        .iter()
-        .rev()
-        .fold(<G1Affine as AffineCurve>::Projective::zero(), |mut total, sum_i| {
-            total += sum_i;
-            for _ in 0..s {
-                total.double_in_place();
-            }
-            total
-        })
-}
-
-pub fn compute_pippenger(
-    point_vec: Vec<<<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::Affine>,
-    scalar_vec: Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>,
-) -> <G1Affine as AffineCurve>::Projective
-{
-    msm::VariableBaseMSM::pippenger_mul(point_vec.as_slice(), scalar_vec.as_slice())
-}
-
-pub fn compute_pippenger_affine(
+pub fn compute_msm_opt(
     point_vec: Vec<<<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::Affine>,
     scalar_vec: Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>,
 ) -> <G1Affine as AffineCurve>::Projective {
-    let (tmp, s)= msm::VariableBaseMSM::pippenger_batch_affine(point_vec.as_slice(),scalar_vec.as_slice());
-    let start = Instant::now();
-    let mut Gs:Vec<_> = ark_std::cfg_into_iter!(tmp).map(|pts|{
-        G1Affine::batch_affine_addition(pts).into_projective()
-    }).collect();
-    let duration = start.elapsed();
-    println!("pippenger+batch | compute_pippenger_affine | batch_affine_addition {:?}", duration);
-    let lowest = *Gs.first().unwrap();
-    // We're traversing windows from high to low.
-    lowest
-        + &Gs[1..]
-        .iter()
-        .rev()
-        .fold(<G1Affine as AffineCurve>::Projective::zero(), |mut total, sum_i| {
-            total += sum_i;
-            for _ in 0..s {
-                total.double_in_place();
-            }
-            total
-        })
+    msm::MultiExp::compute_msm_opt(point_vec.as_slice(), scalar_vec.as_slice())
 }
-
-
-
 
 #[test]
 fn test() {
-    use ark_ff::BigInteger;
-    let size = 1<<14;
+    //use ark_ff::BigInteger;
+    let size = 1 << 16;
     let (point_vec, scalar_vec) = generate_msm_inputs(size);
     //let scalar = <<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt::from_bits_le(&[true,false]);
-    let ww = point_vec.clone();
+    let start = Instant::now();
     let res1 = compute_msm(point_vec.clone(), scalar_vec.clone());
-    let res2 = compute_pippenger(point_vec.clone(), scalar_vec.clone());
-    let res3 = compute_pippenger_affine(point_vec.clone(), scalar_vec.clone());
-    let res4 = compute_msm_affine(point_vec.clone(), scalar_vec.clone());
-    println!("\n baseline = {:?}\n", res1.into_affine());
-    println!("pippenger = {:?}\n", res2.into_affine());
-    println!("pippenger-affine= {:?}\n", res3.into_affine());
-    println!("msm-affine= {:?}\n", res4.into_affine());
+    let duration = start.elapsed();
+    println!("baseline with size 1<<16: {:?}", duration);
+    println!("\n baseline res = {:?}\n", res1.into_affine());
+
+    let start = Instant::now();
+    let res2 = compute_msm_opt(point_vec.clone(), scalar_vec.clone());
+    let duration = start.elapsed();
+    println!("msm_opt with size 1<<16: {:?}", duration);
+    println!("\n msm_opt = {:?}\n", res2.into_affine());
 }
