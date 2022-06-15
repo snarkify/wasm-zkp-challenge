2ff928d7f3c8a9a629e9fbc314fab76ba0ecd1e9
Chao Ma <mc8305@gmail.com>
2022-06-14 20:49:43 -0700

    refactor(minor modification): minor modification on test codes;

--- src/msm.rs
+++ src/msm.rs
@@ -1,6 +1,8 @@
 use ark_bls12_381::G1Affine;
 use ark_ec::{msm, AffineCurve, ProjectiveCurve};
 use ark_ff::{BigInteger, FpParameters, PrimeField, UniformRand, Zero};
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Error, Write};
 use std::time::{Duration, Instant};
 
 pub fn generate_msm_inputs(
@@ -11,7 +13,7 @@ pub fn generate_msm_inputs(
 ) {
     let mut rng = ark_std::test_rng();
 
-    let scalar_vec = (0..size - 1)
+    let scalar_vec = (0..size)
         .map(|_| <G1Affine as AffineCurve>::ScalarField::rand(&mut rng).into_repr())
         .collect::<Vec<_>>();
     let point_vec = (0..size)
@@ -40,21 +42,69 @@ pub fn compute_msm_opt(
     msm::MultiExp::compute_msm_opt(point_vec.as_slice(), scalar_vec.as_slice())
 }
 
+pub fn write_to_file(
+    scalar_vec: Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>,
+    scalar_file: &str,
+) -> Result<(), Error> {
+    let mut output2 = File::create(scalar_file)?;
+    for scalar in scalar_vec {
+        let bits = scalar.to_bits_be();
+        let bits = bits
+            .into_iter()
+            .map(|x| {
+                if x {
+                    format!("{}", 1)
+                } else {
+                    format!("{}", 0)
+                }
+            })
+            .collect::<Vec<String>>()
+            .join("");
+        write!(output2, "{}\n", bits)?;
+    }
+    Ok(())
+}
+
+pub fn read_from_file(
+    file_loc: &str,
+) -> Result<Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>, Error> {
+    let mut scalars = Vec::new();
+    let input = File::open(file_loc)?;
+    let buffered = BufReader::new(input);
+    for line in buffered.lines() {
+        let mut tmp = Vec::new();
+        let line = line?;
+        for c in line.chars() {
+            if c == '0' {
+                tmp.push(false);
+            } else {
+                tmp.push(true);
+            }
+        }
+        let scalar =
+            <<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt::from_bits_be(&tmp);
+        scalars.push(scalar);
+    }
+    Ok(scalars)
+}
+
 #[test]
 fn test() {
-    //use ark_ff::BigInteger;
-    let size = 1 << 16;
+    let K = 16;
+    let size = 1 << K;
     let (point_vec, scalar_vec) = generate_msm_inputs(size);
+    let _ = write_to_file(scalar_vec.clone(), "./scalar.txt");
     //let scalar = <<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt::from_bits_le(&[true,false]);
+    //let scalar_vec1 = read_from_file("./scalar.txt").unwrap();
     let start = Instant::now();
     let res1 = compute_msm(point_vec.clone(), scalar_vec.clone());
     let duration = start.elapsed();
-    println!("baseline with size 1<<16: {:?}", duration);
+    println!("baseline with size 1<<{}: {:?}", K, duration);
     println!("\n baseline res = {:?}\n", res1.into_affine());
 
     let start = Instant::now();
     let res2 = compute_msm_opt(point_vec.clone(), scalar_vec.clone());
     let duration = start.elapsed();
-    println!("msm_opt with size 1<<16: {:?}", duration);
+    println!("msm_opt with size 1<<{}: {:?}", K, duration);
     println!("\n msm_opt = {:?}\n", res2.into_affine());
 }
